b114f55ad4b5fca85f7a39eda60c3974c77d6d20
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-07-14 15:24:03 -0500

    removed stuff

--- pages/book/[firebaseKey].js
+++ pages/book/[firebaseKey].js
@@ -1,38 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { viewBookDetails } from '../../api/mergedData';
+// import React from 'react';
 
-export default function ViewBook() {
-  const [bookDetails, setBookDetails] = useState({});
-  const router = useRouter();
+// export default function ViewBook() {
+//   // TODO: grab firebaseKey from url
+//   // TODO: make call to API layer to get the data
 
-  // TODO: grab firebaseKey from url
-  const { firebaseKey } = router.query;
-
-  // TODO: make call to API layer to get the data
-  useEffect(() => {
-    viewBookDetails(firebaseKey).then(setBookDetails);
-  }, [firebaseKey]);
-
-  return (
-    <div className="mt-5 d-flex flex-wrap">
-      <div className="d-flex flex-column">
-        <img src={bookDetails.image} alt={bookDetails.title} style={{ width: '300px' }} />
-      </div>
-      <div className="text-white ms-5 details">
-        <h5>
-          {bookDetails.title} by {bookDetails.authorObject?.first_name} {bookDetails.authorObject?.last_name}
-          {bookDetails.authorObject?.favorite ? ' 🤍' : ''}
-        </h5>
-        Author Email: <a href={`mailto:${bookDetails.authorObject?.email}`}>{bookDetails.authorObject?.email}</a>
-        <p>{bookDetails.description || ''}</p>
-        <hr />
-        <p>
-          {bookDetails.sale
-            ? `🏷️ Sale $${bookDetails.price}`
-            : `$${bookDetails.price}`}
-        </p>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="mt-5 d-flex flex-wrap">
+//       <div className="d-flex flex-column">
+//         <img src={bookDetails.image} alt={bookDetails.title} style={{ width: '300px' }} />
+//       </div>
+//       <div className="text-white ms-5 details">
+//         <h5>
+//           {bookDetails.title} by {bookDetails.authorObject?.first_name} {bookDetails.authorObject?.last_name}
+//           {bookDetails.authorObject?.favorite ? ' 🤍' : ''}
+//         </h5>
+//         Author Email: <a href={`mailto:${bookDetails.authorObject?.email}`}>{bookDetails.authorObject?.email}</a>
+//         <p>{bookDetails.description || ''}</p>
+//         <hr />
+//         <p>
+//           {bookDetails.sale
+//             ? `🏷️ Sale $${bookDetails.price}`
+//             : `$${bookDetails.price}`}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
--- pages/book/edit/[firebaseKey].js
+++ pages/book/edit/[firebaseKey].js
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { getSingleBook } from '../../../api/bookData';
+import React from 'react';
 import BookForm from '../../../components/forms/BookForm';
 
 export default function EditBook() {
-  const [editItem, setEditItem] = useState({});
-  const router = useRouter();
   // TODO: grab the firebasekey
-  const { firebaseKey } = router.query;
-
   // TODO: make a call to the API to get the book data
-  useEffect(() => {
-    getSingleBook(firebaseKey).then(setEditItem);
-  }, [firebaseKey]);
-
   // TODO: pass object to form
-  return (<BookForm obj={editItem} />);
+  return (<BookForm />);
 }
