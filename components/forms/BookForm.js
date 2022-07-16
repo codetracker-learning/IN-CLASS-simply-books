4079cad4fe7bcb3878b7ce9c115868958c182170
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-07-16 14:48:49 -0500

    updated form

--- components/forms/BookForm.js
+++ components/forms/BookForm.js
@@ -52,22 +52,51 @@ function BookForm({ obj }) {
   return (
     <Form onSubmit={handleSubmit}>
       <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Book</h2>
+
+      {/* TITLE INPUT  */}
       <FloatingLabel controlId="floatingInput1" label="Book Title" className="mb-3">
-        <Form.Control type="text" placeholder="Enter a title" name="title" value={formInput.title} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingInput2" label="Book Image" className="mb-3">
-        <Form.Control type="url" placeholder="Enter an image url" name="image" value={formInput.image} onChange={handleChange} required />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingInput3" label="Book Price" className="mb-3">
-        <Form.Control type="text" placeholder="Enter price" name="price" value={formInput.price} onChange={handleChange} required />
+        <Form.Control
+          type="text"
+          placeholder="Enter a title"
+          name="title"
+          value={formInput.title}
+          onChange={handleChange}
+          required
+        />
       </FloatingLabel>
 
+      {/* IMAGE INPUT  */}
+      <FloatingLabel controlId="floatingInput2" label="Book Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Enter an image url"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* PRICE INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="Book Price" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter price"
+          name="price"
+          value={formInput.price}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* AUTHOR SELECT  */}
       <FloatingLabel controlId="floatingSelect" label="Author">
         <Form.Select
           aria-label="Author"
           name="author_id"
           onChange={handleChange}
           className="mb-3"
+          value={obj.author_id} // FIXME: modify code to remove error
           required
         >
           <option value="">Select an Author</option>
@@ -76,7 +105,6 @@ function BookForm({ obj }) {
               <option
                 key={author.firebaseKey}
                 value={author.firebaseKey}
-                selected={obj.author_id === author.firebaseKey}
               >
                 {author.first_name} {author.last_name}
               </option>
@@ -85,8 +113,17 @@ function BookForm({ obj }) {
         </Form.Select>
       </FloatingLabel>
 
+      {/* DESCRIPTION TEXTAREA  */}
       <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
-        <Form.Control as="textarea" placeholder="Description" style={{ height: '100px' }} name="description" value={formInput.description} onChange={handleChange} required />
+        <Form.Control
+          as="textarea"
+          placeholder="Description"
+          style={{ height: '100px' }}
+          name="description"
+          value={formInput.description}
+          onChange={handleChange}
+          required
+        />
       </FloatingLabel>
 
       {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
@@ -97,11 +134,15 @@ function BookForm({ obj }) {
         name="sale"
         label="On Sale?"
         checked={formInput.sale}
-        onChange={(e) => setFormInput((prevState) => ({
-          ...prevState,
-          sale: e.target.checked,
-        }))}
+        onChange={(e) => {
+          setFormInput((prevState) => ({
+            ...prevState,
+            sale: e.target.checked,
+          }));
+        }}
       />
+
+      {/* SUBMIT BUTTON  */}
       <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Book</Button>
     </Form>
   );
