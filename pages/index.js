56a4cd79a6516a8b43709a402d00e45ae4a12db8
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-07-16 16:46:40 -0500

    possible stretch

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ function Home() {
   // TODO: create a function that makes the API call to get all the books
   // TODO: make the call to the API to get all the books on component render
 
+  // TODO: STRETCH - for search...
+
   return (
     <div className="text-center my-4">
       <Link href="/book/new" passHref>
